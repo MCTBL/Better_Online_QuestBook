7ce35d470d5bda59cdd1b24a8dc0deb5e6322993
NoRain <wuxinrufeng@qq.com>
2025-01-10 15:25:36 +0800

    初次提交

---
+++ READMD.md
@@ -0,0 +1,7 @@
+# Better_Online_QuestBook
+
+## 2025/1/10
+
+## *by MCTBL & Grievous_Rain*
+
+先搭建个大概,整理下思路,有空再做,计划在4月份之前完工()
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export class index {
+    constructor() {
+        console.log('Hello Better_Online_QuestBook');
+    }
+}
+new index();
