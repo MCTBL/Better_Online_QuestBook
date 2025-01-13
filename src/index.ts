2cf92679fd456171cb0a081e7fbd9e861318dcdf
NoRain <wuxinrufeng@qq.com>
2025-01-13 19:57:29 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-
-var win = window;
-var $ = (window as any).$;
-
 export class index {
 
     private questList: quest[] = [];
+    private buttonList: JQuery<HTMLElement>[] = [];
     constructor() {
+        this.addEvent();
+        this.loadList();
+    }
+
+    addEvent() {
+        $("#search").on("input", (e) => {
+            const text = $(e.target)!.val()!.toString();
+            $(".questButton").each((index, element) => {
+                const button = $(element);
+                const questData = button.data("questData");
+                if (questData.title.includes(text) || questData.title_zh?.includes(text) || questData.search_key?.includes(text)) {
+                    button.show();
+                } else {
+                    button.hide();
+                }
+            });
+        });
+    }
+
+    loadList() {
         $(document).ready(() => {
             $.getJSON("./res/quest_line.json", (data: any) => {
                 this.questList = data;
                 this.questList.forEach((quest, index) => {
                     let button = this.createButton(index, quest);
                     $("#sidebar").append(button);
+                    this.buttonList.push(button);
                 });
             });
         });
-
-
-
-
     }
 
     createButton(index: number, quest: quest) {
@@ -29,6 +43,7 @@ export class index {
                 console.log(quest.url);
             }
         });
+        button.data("questData", quest);
 
         const img = $('<img>', {
             src: "./icon/" + quest.icon + ".png",
@@ -36,7 +51,7 @@ export class index {
         });
 
         const txt = $('<span>', {
-            text: quest.title,
+            text: quest.title_zh ? quest.title_zh : quest.title,
             class: 'questText'
         });
 
@@ -47,7 +62,7 @@ export class index {
 
 
     sendMessageToIframe(msg: any) {
-        $("#mainIframe").contentWindow.postMessage(msg, "*");
+
     }
 
 
@@ -55,4 +70,4 @@ export class index {
 
 new index();
 
-type quest = { title: string, url: string, icon: string }
+type quest = { title: string, url: string, icon: string, title_zh?: string, search_key?: string[] }
