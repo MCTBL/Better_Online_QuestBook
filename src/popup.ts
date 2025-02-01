debabfedb4feb07eafc6765d5bb964e7e805f713
MCTBL <wszth0130@outlook.com>
2025-02-01 18:16:14 +0800

    finish esc and e close popup

--- src/popup.ts
+++ src/popup.ts
@@ -8,6 +8,7 @@ export function showPopup(title: string, desc: string, ID: string) {
 		document.getElementById("desc")!.innerHTML = processDesc(desc);
 		document.getElementById("popup")!.style.display = "flex";
 		document.getElementById("overlay")!.style.display = "block";
+		document.getElementById("mainPage")!.focus();
 	}
 }
 
