5ccca8e4db3f91249d100355fcc9a9eea3cabfa7
MCTBL <2772337924@qq.com>
2025-02-12 12:13:03 +0800

    add quest background

--- src/ProjectData.ts
+++ src/ProjectData.ts
@@ -1,15 +1,20 @@
 import { ProjectConfig } from "./ProjectConfig.js";
 
 export class ProjectData {
-    static selectVersionIndex: number = 0;
-    static getPath(url: string) {
-        return "version/" + ProjectConfig.resList[ProjectData.selectVersionIndex] + "/" + url;
-    }
+	static selectVersionIndex: number = 0;
+	static getPath(url: string) {
+		return (
+			"version/" +
+			ProjectConfig.resList[ProjectData.selectVersionIndex] +
+			"/" +
+			url
+		);
+	}
 
 	/**获取任务列表数据地址 */
-    static getQuestLinePath() {
-        return this.getPath(ProjectConfig.questLinePath);
-    }
+	static getQuestLinePath() {
+		return this.getPath(ProjectConfig.questLinePath);
+	}
 
 	/**获取任务数据地址 */
 	static getQuestDataPath() {
@@ -18,19 +23,19 @@ export class ProjectData {
 
 	/**默认echarts配置 */
 	static readonly echartsConfig = {
-		"backgroundColor": "#f5f0d3",
-		"animation": true,
-		"animationThreshold": 2000,
-		"animationDuration": 1000,
-		"animationEasing": "cubicOut",
-		"animationDelay": 0,
-		"animationDurationUpdate": 300,
-		"animationEasingUpdate": "cubicOut",
-		"animationDelayUpdate": 0,
-		"aria": {
-			"enabled": false
+		backgroundColor: "#f5f0d3",
+		animation: true,
+		animationThreshold: 2000,
+		animationDuration: 1000,
+		animationEasing: "cubicOut",
+		animationDelay: 0,
+		animationDurationUpdate: 300,
+		animationEasingUpdate: "cubicOut",
+		animationDelayUpdate: 0,
+		aria: {
+			enabled: false,
 		},
-		"color": [
+		color: [
 			"#5470c6",
 			"#91cc75",
 			"#fac858",
@@ -39,81 +44,77 @@ export class ProjectData {
 			"#3ba272",
 			"#fc8452",
 			"#9a60b4",
-			"#ea7ccc"
+			"#ea7ccc",
 		],
-		"series": [
+		series: [
 			{
-				"type": "graph",
-				"layout": "none",
-				"symbolSize": 10,
-				"circular": {
-					"rotateLabel": false
+				type: "graph",
+				layout: "none",
+				symbolSize: 10,
+				circular: {
+					rotateLabel: false,
 				},
-				"force": {
-					"repulsion": 0,
-					"gravity": 0,
-					"edgeLength": 30,
-					"friction": 0.6,
-					"layoutAnimation": true
+				force: {
+					repulsion: 0,
+					gravity: 0,
+					edgeLength: 30,
+					friction: 0.6,
+					layoutAnimation: true,
 				},
-				"label": {
-					"show": false,
-					"position": "up",
-					"margin": 8,
-					"valueAnimation": false
+				label: {
+					show: false,
+					position: "up",
+					margin: 8,
+					valueAnimation: false,
 				},
-				"lineStyle": {
-					"show": true,
-					"width": 1,
-					"opacity": 1,
-					"curveness": 0,
-					"type": "solid"
+				lineStyle: {
+					show: true,
+					width: 1,
+					opacity: 1,
+					curveness: 0,
+					type: "solid",
 				},
-				"roam": true,
-				"draggable": false,
-				"focusNodeAdjacency": true,
-				"data": null,
-				"edgeLabel": {
-					"show": false,
-					"margin": 8,
-					"valueAnimation": false
+				roam: true,
+				draggable: false,
+				focusNodeAdjacency: true,
+				data: null,
+				edgeLabel: {
+					show: false,
+					margin: 8,
+					valueAnimation: false,
 				},
-				"edgeSymbol": [
-					"circle",
-					"arrow"
-				],
-				"edgeSymbolSize": 10,
-				"links": null,
-			}
+				edgeSymbol: ["circle", "arrow"],
+				edgeSymbolSize: 10,
+				links: null,
+			},
 		],
-		"legend": [
+		legend: [
 			{
-				"data": [],
-				"selected": {}
-			}
+				data: [],
+				selected: {},
+			},
 		],
-		"tooltip": {
-			"show": true,
-			"trigger": "item",
-			"triggerOn": "mousemove|click",
-			"axisPointer": {
-				"type": "line"
+		tooltip: {
+			show: true,
+			trigger: "item",
+			triggerOn: "mousemove|click",
+			axisPointer: {
+				type: "line",
 			},
-			"showContent": true,
-			"alwaysShowContent": false,
-			"showDelay": 0,
-			"hideDelay": 100,
-			"enterable": false,
-			"confine": false,
-			"appendToBody": false,
-			"transitionDuration": 0.4,
-			"textStyle": {
-				"fontSize": 14
+			showContent: true,
+			alwaysShowContent: false,
+			showDelay: 0,
+			hideDelay: 100,
+			enterable: false,
+			confine: false,
+			appendToBody: false,
+			transitionDuration: 0.4,
+			textStyle: {
+				fontSize: 14,
 			},
-			"borderWidth": 0,
-			"padding": 5,
-			"order": "seriesAsc"
-		}
+			borderWidth: 0,
+			padding: 5,
+			order: "seriesAsc",
+		},
 	};
-
 }
